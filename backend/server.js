de0484d0fb9c2e042b4fbabc13c95b9dfff9eb8d
Samuel Andrew <breeze@pop-os.localdomain>
2021-01-06 11:03:53 +0100

    Added a dotenv file

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,25 @@
 import express from "express";
+import dotenv from "dotenv";
+import products from "./data/products.js";
 
 const app = express();
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.warn(`SERVER RUNNING IN ${process.env.NODE_ENV} ON PORT ${PORT}`)
+);
+
+app.get("/", (req, res) => {
+  res.json("WELCOME");
+});
+
+app.get("/api/products", (req, res) => {
+  res.status(200).json(products);
+});
+
+app.get("/api/product/:id", (req, res) => {
+  const product = products.find((prod) => prod._id === req.params.id);
+  res.status(200).json(product);
+});
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,21 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import { Button, Card, Col, Image, ListGroup, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Rating from "../components/Rating";
-import products from "../products";
 
 const ProductScreen = ({ match }) => {
-  const product = products.find((prod) => prod._id === match.params.id);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/product/${match.params.id}`);
+      setProduct(data);
+    };
+
+    fetchProduct();
+  }, [match.params.id]);
+
   return (
     <>
       <Link className="btn btn-light my-3">Go Back</Link>
